7a48216c5338f73070ea908d571d8921e1eb5afd
kaivanenko.mail.ru <kaivanenko@mail.ru>
2024-01-16 23:27:44 +0300

    test css

--- src/App.js
+++ src/App.js
@@ -7,6 +7,29 @@ function App() {
       <header className="App-header">
         Test 1
       </header>
+      
+      <div className="item">Hu</div>
+
+      <div className="item"> ThreeJs </div>
+
+      <div className="item"> Project </div>
+
+      <div className="item">Transport</div>
+
+      <div className='item'> Car 1 </div>
+
+      <div className='item'> Train </div>
+
+      <div className='item'> Car 2 </div>
+
+      <div className='item'> Маршрут </div> 
+
+      <div> </div>
+
+      <div className='item'> Support </div>
+
+
+
     </div>
   );
 }
